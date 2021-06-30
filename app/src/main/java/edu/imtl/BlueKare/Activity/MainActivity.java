6905fe28192c61531e91cf2050b0cc1e6d634fa8
Brian-Hwang <brian11hwangb@gmail.com>
2021-06-30 10:54:29 +0900

    main activity change

--- app/src/main/java/edu/imtl/BlueKare/Activity/MainActivity.java
+++ app/src/main/java/edu/imtl/BlueKare/Activity/MainActivity.java
@@ -51,9 +51,7 @@ import static edu.imtl.BlueKare.Activity.MainPackage.fragment2_test.splist;
 import static edu.imtl.BlueKare.Activity.MainPackage.fragment2_test.tData;
 import static edu.imtl.BlueKare.Activity.MainPackage.fragment2_test.tList;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    BottomNavigationView bnv;
-    NavController  nc;
+public class MainActivity extends AppCompatActivity{
     FloatingActionButton btn;
     DrawerLayout drawerLayout;
     public static FirebaseFirestore fstore;
@@ -63,23 +61,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public static String finalUserId;
     public static int datasize;
     TextView musername, museremail;
-    private long lastTimeBackPressed;;
+    private long lastTimeBackPressed;
+    ;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     public static String userName;//세중 추가
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        //네비게이션 바
-//        bnv = (BottomNavigationView)findViewById(R.id.bottomNavigation);
-//        nc = Navigation.findNavController(this,R.id.fragment);
-//        NavigationUI.setupWithNavController(bnv,nc);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
-        setNavigationViewListener();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
 
         //AR 버튼
         btn = findViewById(R.id.arbutton);
@@ -97,16 +90,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
         mFirebaseAuth = FirebaseAuth.getInstance();
         fstore = FirebaseFirestore.getInstance();
-        userID= mFirebaseAuth.getCurrentUser().getUid();
+        userID = mFirebaseAuth.getCurrentUser().getUid();
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigationlayout);
         View headerView = navigationView.getHeaderView(0);
-        museremail=(TextView) headerView.findViewById(R.id.userdraweremail);
-        musername=(TextView) headerView.findViewById(R.id.userdrawername);
-        //세중추가------------------------------------------
-        tData = TreesContent.getTrees();
-        tList = TreesContent.getTrees().getAllTrees();
-        //세중추가------------------------------------------
-
+        museremail = (TextView) headerView.findViewById(R.id.userdraweremail);
+        musername = (TextView) headerView.findViewById(R.id.userdrawername);
 
         DocumentReference docRef = fstore.collection("users").document(userID);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -117,18 +105,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     if (document.exists()) {
                         musername.setText(document.getString("fName"));
                         museremail.setText(document.getString("email"));
-                        finalUserId=userID;
-                        thesize(finalUserId);
-                        getgeo(finalUserId);
+                        finalUserId = userID;
                     }
-                }
-                else {
+                } else {
                     Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
 
         findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,209 +121,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-
-        //search
-        mSearchBtn = (ImageButton) findViewById(R.id.search_go_btn);
-        mSearchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this , SearchActivity.class));
-                finish();
-            }
-        });
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragment2_test()).commit();
-            navigationView.setCheckedItem(R.id.fragment2_test_nav);
-        }
-    }
-    public static void getgeo(String userid)
-    {
-
-        System.out.print("아이디 = " + userid);//성공
-        fstore.collection("tree").document(userid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        List<String> listS = new ArrayList<>();
-                        List<Object> listO = new ArrayList<>();
-
-                        Map<String, Object> map = document.getData();
-                        if (map != null) {
-                            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                                String key=entry.getKey();
-                                Object object=entry.getValue();
-                                listS.add(key);
-                                listO.add(object);
-                                System.out.print("키값들 : "+key);
-                            }
-                        }
-
-                        for (Object s : listO) {
-                            Map<String,Object> map2=(Map)s;
-                            for(Map.Entry<String,Object> entry:map2.entrySet())
-                            {
-                                GeoPoint fgeo=null;
-                                String keyname=entry.getKey();
-                                Object objname=entry.getValue();
-                                System.out.print("key값들 : "+keyname+"원소들 : "+objname);
-                                if(keyname.equals("treeLocation"))
-                                {
-                                    fgeo=(GeoPoint)objname;
-                                    System.out.print("포인트 과연? "+fgeo);
-                                    geolist.add(fgeo);
-                                }
-                                else if(keyname.equals("treeName"))
-                                {
-                                    String name=(String)objname;
-                                    namelist.add(name);
-                                }
-                                else if(keyname.equals("treeDBH"))
-                                {
-                                    String fi=(String)objname;
-                                    Double dbh=Double.parseDouble(fi);
-                                    dbhlist.add(dbh);
-                                }
-                                else if(keyname.equals("treeSpecies"))
-                                {
-                                    String sp=(String)objname;
-                                    splist.add(sp);
-                                }
-                                else if(keyname.equals("treeHeight"))
-                                {
-                                    if(objname!=null) {
-                                        String sp = (String) objname;
-                                        Double hei = Double.parseDouble(sp);
-                                        helist.add(hei);
-                                    }
-                                }
-                            }
-                        }
-                        System.out.println("   리스트  "+geolist);
-                    }
-                }
-            }
-        });
-    }
-    public static void thesize(String userid)
-    {
-        System.out.print(userid);
-        fstore.collection("tree").document(userid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        List<String> listS = new ArrayList<>();
-                        Map<String, Object> map = document.getData();
-                        if (map != null) {
-                            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                                String key=entry.getKey();
-                                listS.add(key);
-                            }
-                            datasize=listS.size();
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        switch (item.getItemId()) {
-            case R.id.Logout: {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this , LoginActivity.class));
-                this.finish();
-                break;
-            }
-            case R.id.fragment2_test_nav: {
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragment2_test()).commit();
-                break;
-            }
-            case R.id.fragment3_nav: {
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragment3()).commit();
-                break;
-            }
-            case R.id.History_nav: {
-                //Toast.makeText(this, "History", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this , SearchActivity.class));
-                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new fragment3());
-                break;
-            }
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerlayout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    public static class App extends Application {
-
-        private static Context mContext;
-
-        @Override
-        public void onCreate() {
-            super.onCreate();
-            mContext = this;
-        }
-
-        public static Context getContext(){
-            return mContext;
-        }
-    }
-
-
-    public void onResume() {
-        super.onResume();
-        {
-            if (!LocationHelper.hasLocationPermission(this)) {
-                LocationHelper.requestLocationPermission(this);
-            }
-          //  getgeo(finalUserId);
-        }
     }
 
 
     @Override
     public void onBackPressed() {
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
-        if(drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
             drawerLayout.closeDrawer(Gravity.LEFT);
-        }
-        else {
+        } else {
             //super.onBackPressed();
-            if (System.currentTimeMillis() - lastTimeBackPressed < 2000)
-            {
+            if (System.currentTimeMillis() - lastTimeBackPressed < 2000) {
                 finish();
                 return;
             }
             Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르시면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show();
             lastTimeBackPressed = System.currentTimeMillis();
         }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (!LocationHelper.hasLocationPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                    .show();
-            if (!LocationHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                LocationHelper.launchPermissionSettings(this);
-            }
-            LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            finish();
-        }
-    }
-
-    private void setNavigationViewListener() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationlayout);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 }
 

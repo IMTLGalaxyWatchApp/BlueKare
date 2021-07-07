7d7ccfe2735ded9d76cdbc552073aa18f32fb487
Brian-Hwang <brian11hwangb@gmail.com>
2021-07-07 17:29:04 +0900

    record 이식 완료

---
+++ app/src/main/java/edu/imtl/BlueKare/Activity/Record/Fragment_record_audiolistAdapter.java
@@ -0,0 +1,75 @@
+package edu.imtl.BlueKare.Activity.Record;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import edu.imtl.BlueKare.R;
+
+
+public class Fragment_record_audiolistAdapter extends RecyclerView.Adapter<Fragment_record_audiolistAdapter.AudioViewHolder> {
+
+    private File[] allFiles;
+    private Fragment_record_TimeAgo timeAgo;
+
+    private onItemListClick onItemListClick;
+
+    public Fragment_record_audiolistAdapter(File[] allFiles, onItemListClick onItemListClick) {
+        this.allFiles = allFiles;
+        this.onItemListClick = onItemListClick;
+    }
+
+    @NonNull
+    @Override
+    public AudioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R .layout.single_list_item, parent, false);
+        timeAgo = new Fragment_record_TimeAgo();
+        return new AudioViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull AudioViewHolder holder, int position) {
+        holder.list_title.setText(allFiles[position].getName());
+        holder.list_date.setText(timeAgo.getTimeAgo(allFiles[position].lastModified()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return allFiles.length;
+    }
+
+    public class AudioViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView list_image;
+        private TextView list_title;
+        private TextView list_date;
+
+        public AudioViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            list_image = itemView.findViewById(R.id.list_image_view);
+            list_title = itemView.findViewById(R.id.list_title);
+            list_date = itemView.findViewById(R.id.list_date);
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            onItemListClick.onClickListener(allFiles[getAdapterPosition()], getAdapterPosition());
+        }
+    }
+
+    public interface onItemListClick {
+        void onClickListener(File file, int position);
+    }
+
+}
